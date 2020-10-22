baa10ab99551bdc7c021d1dc6e3fd62f45c455aa
alejandro.monux <alejandro.monux@students.salle.url.edu>
2020-10-22 12:51:42 +0200

    Fix: Compilation and execution errors fixed

--- main.c
+++ main.c
@@ -6,17 +6,16 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <signal.h>
+#include <fcntl.h>
 
 //Strings necessaris.
 #define FILE_ERROR "ERROR. ARXIU DE CONFIGURACIÓ DE %s NO TROBAT"
 #define ARGUMENT_ERROR "ERROR. NO S'HAN TROBAT SUFICIENTS ARGUMENTS DE PROGRAMA."
 #define ERROR_RETURN -1
 
-//Variables globals
-configDanny config;
-
 //Estructura que guardarà la informació de configuració de Danny
-typedef struct ConfigDanny{
+typedef struct configDanny{
   char * nom;
   char * carpeta;
   int temps;
@@ -24,7 +23,12 @@ typedef struct ConfigDanny{
   int portJack;
   char * ipWendy;
   int portWendy;
-}
+}configDanny;
+
+//Variables globals
+configDanny config;
+
+
 
 
 void signalhandler(int sigint){
@@ -42,20 +46,21 @@ void signalhandler(int sigint){
 *Retorna: La cadena llegida
 */
 char * LlegirCadena(int fd){
-  char *cadena;
+  char *cadena = (char *) malloc(sizeof(char)*1);
   char buff;
   int lletres = 0;
-
-  read(fd, buff, sizeof(char));
-  if(buff == '\n'){
-      return(cadena)
-  }else{
-    lletres++;
-    cadena = (char *) realloc(cadena, sizeof(char)*lletres)
-    cadena[lletres-1] = buff;
+  int comprovacio;
+  while (1){
+    comprovacio = read(fd, &buff, sizeof(char));
+    if((buff == '\n')||(comprovacio <= 0)){
+        return(cadena);
+    }else{
+      lletres++;
+      cadena = (char *) realloc(cadena, sizeof(char)*lletres);
+      cadena[lletres-1] = buff;
+    }
   }
-
-  return(cadena)
+  //return(cadena);
 }
 
 
@@ -87,10 +92,14 @@ configDanny llegirConfig(char *nomFitxer){
   read(fitxer,&(config).temps,sizeof(int));
   //Llegim les dades de Jack
   config.ipJack =  LlegirCadena(fitxer);
-  read(fitxer,&(config).portJack,sizeof(int));
+  char * aux = LlegirCadena(fitxer);
+  config.portJack= atoi(aux);
+  free(aux);
   //Llegim les dades de Wendy
   config.ipWendy =  LlegirCadena(fitxer);
-  read(fitxer,&(config).portWendy,sizeof(int));
+  aux = LlegirCadena(fitxer);
+  config.portWendy = atoi(aux);
+  free(aux);
 
   //Tanquem el File Descriptor
   close(fitxer);
@@ -98,16 +107,27 @@ configDanny llegirConfig(char *nomFitxer){
   return(config);
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
     if(argc < 2){
       char buff[100];
       int bytes = sprintf(buff, ARGUMENT_ERROR);
       write(1, buff, bytes);
       exit(ERROR_RETURN);
     }
-    config = llegirConfig(argv[1])
+    config = llegirConfig(argv[1]);
+    printf("NOM: %s\n", config.nom);
+    printf("CARPETA: %s\n", config.carpeta);
+    printf("TEMPS: %d\n", config.temps);
+    printf("ipJack: %s\n", config.ipJack);
+    printf("port Jack: %d\n", config.portJack);
+    printf("ipWendy: %s\n", config.ipWendy);
+    printf("port Wendy: %d\n", config.portWendy);
+
     signal(SIGALRM, signalhandler);
 
-
+    free (config.nom);
+    free (config.carpeta);
+    free (config.ipJack);
+    free (config.ipWendy);
     return 0;
 }
