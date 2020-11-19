dc36cf191f4f913adf9cc98371dbd200ca70a629
alejandro.monux <alejandro.monux@students.salle.url.edu>
2020-11-19 15:44:49 +0100

    Update socket.h

--- connectionUtils/socket.h
+++ connectionUtils/socket.h
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include "fileParser.h"
 
 #define IP_ERROR "%s no es una adreça IP vàlida\n"
 #define BIND_ERROR "Error en fer el Bind!\n"
