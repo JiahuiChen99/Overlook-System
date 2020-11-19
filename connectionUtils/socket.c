52cd491b0927abb7d3014bf0c28460cf81eaa09a
alejandro.monux <alejandro.monux@students.salle.url.edu>
2020-11-19 11:38:56 +0100

    Feat: Creats sockets

---
+++ connectionUtils/socket.c
@@ -0,0 +1,33 @@
+#include "socket.h"
+
+int iniciarServidor(char *ip, int port){
+  char buff[100];
+  int bytes
+  //Creem el socket
+  int socketFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+  //Preparem la estructura
+  struct sockaddr_in s_addr;
+  memset (&s_addr, 0, sizeof (s_addr));
+  s_addr.sin_family = AF_INET;
+  s_addr.sin_port = htons (port);
+
+  if (inet_aton (argv[1], &s_addr.sin_addr) == 0){
+    bytes = sprintf(buff, IP_ERROR, ip);
+    write(1, buff, bytes);
+    return -1;
+  }
+  //Executem el bind per indicar que esperem rebre info
+  if(bind(socketFD, &s_addr, sizeof(s_addr)) < 0){
+    bytes = sprintf(buff, BIND_ERROR);
+    write(1, buff, bytes);
+    return -1;
+  }
+
+  if(listen(socketFD, 3)){
+    bytes = sprintf(buff, LISTEN_ERROR);
+    write(1, buff, bytes);
+    return -1;
+  }
+
+}
---
+++ connectionUtils/socket.h
@@ -0,0 +1,18 @@
+#ifndef OVERLOOK_SYSTEM_SOCKETS_H
+#define OVERLOOK_SYSTEM_SOCKETS_H
+#define _DEFAULT_SOURCE
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define IP_ERROR "%s no es una adreça IP vàlida\n"
+#define BIND_ERROR "Error en fer el Bind!\n"
+#define LISTEN_ERROR "Error en fer el Listen!\n"
+#define ACCEPT_ERROR "Error en fer el Accept!\n"
+/*
+*Iniciar el servidor amb la ip i port donats
+*
+*Returns: File Descriptor del socket
+*/
+int iniciarServidor(char *ip, int port);
