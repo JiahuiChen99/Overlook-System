c00326eead3b990b7c0a65b50cdddda960e5c158
alejandro.monux <alejandro.monux@students.salle.url.edu>
2020-12-05 12:50:06 +0100

    Compilation problems fixed (dependency problems not)

--- Lloyd/lloyd.c
+++ Lloyd/lloyd.c
@@ -28,20 +28,21 @@ infoLloyd * memComp;
 infoLloyd * estacions; //Array de les mitjanes de estacions
 infoLloyd * infoAcumulada; //Matriu on guardem totes les instàncies de info que ens arriben
 int * numDades; //Guardem quantes dades té cada array de infoAcumulada
+int numEstacions;
 
 //Signal Handler per a controlar ctrl+c que faràn sortir el programa
-int signalhandler(int signum){
+void signalhandler(int signum){
   switch(signum){
     case SIGINT:
       //Destruim semàfor
-      SEM_destructor(semJack);
+      SEM_destructor(&semJack);
       //Netejem la memoria compartida
       shmdt(memComp);
       shmctl(shm, IPC_RMID, NULL);
       //Alliberem la memòria reservada.
-      for(int i = 0; i < numDades; i++){
-        free(estacions[i].nomEstacio);
-        free(infoAcumulada[i].nomEstacio);
+      for (int j = 0; j < numEstacions; j++){
+          free(estacions[j].nomEstacio);
+          free(infoAcumulada[j].nomEstacio);
       }
       free(estacions);
       free(infoAcumulada);
@@ -51,7 +52,7 @@ int signalhandler(int signum){
       break;
   }
   signal(signum, signalhandler);
-  return 0;
+  //return 0;
 }
 
 int buscaEstacio(char * estacio, int numEstacions){
@@ -73,66 +74,65 @@ void calculaMitjana(int estacio){
 
 void guardaDadesMitjana(int estacio){
   //TODO:Fer-ho amb strcpy
-  estacions[estacio].nomEstacio = (char *) malloc(strlen(memComp.nomEstacio));
-  strcpy(infoAcumulada[estacio].nomEstacio, memComp.nomEstacio);
-  estacions[estacio].temperatura = memComp.temperatura;
-  estacions[estacio].humitat = memComp.humitat;
-  estacions[estacio].pressio_atmosferica = memComp.pressio_atmosferica;
-  estacions[estacio].precipitacio = memComp.precipitacio;
+  estacions[estacio].nomEstacio = (char *) malloc(strlen(memComp->nomEstacio));
+  strcpy(infoAcumulada[estacio].nomEstacio, memComp->nomEstacio);
+  estacions[estacio].temperatura = memComp->temperatura;
+  estacions[estacio].humitat = memComp->humitat;
+  estacions[estacio].pressio_atmosferica = memComp->pressio_atmosferica;
+  estacions[estacio].precipitacio = memComp->precipitacio;
 }
 
 void guardaDadesAcumulades(int estacio){
   //TODO:Fer-ho amb strcpy
-  infoAcumulada[estacio].nomEstacio = (char *) malloc(strlen(memComp.nomEstacio));
-  strcpy(infoAcumulada[estacio].nomEstacio, memComp.nomEstacio);
-  infoAcumulada[estacio].temperatura += memComp.temperatura;
-  infoAcumulada[estacio].humitat += memComp.humitat;
-  infoAcumulada[estacio].pressio_atmosferica += memComp.pressio_atmosferica;
-  infoAcumulada[estacio].precipitacio += memComp.precipitacio;
+  infoAcumulada[estacio].nomEstacio = (char *) malloc(strlen(memComp->nomEstacio));
+  strcpy(infoAcumulada[estacio].nomEstacio, memComp->nomEstacio);
+  infoAcumulada[estacio].temperatura += memComp->temperatura;
+  infoAcumulada[estacio].humitat += memComp->humitat;
+  infoAcumulada[estacio].pressio_atmosferica += memComp->pressio_atmosferica;
+  infoAcumulada[estacio].precipitacio += memComp->precipitacio;
+}
+
+void inicialitzaAcum(int index){
+  infoAcumulada[index].temperatura = 0;
+  infoAcumulada[index].humitat = 0;
+  infoAcumulada[index].pressio_atmosferica = 0;
+  infoAcumulada[index].precipitacio = 0;
 }
 
 int main(){
-  int nBytes, numEstacions = 0;
-  char buff[150];
+  numEstacions = 0;
   //Agafem la memòria compartida
   //shmget
-  shm = shmget(ftok("lloyd.c", "a"), sizeof(infoLloyd), IPC_CREAT | IPD_EXCL | 0600);
+  shm = shmget(ftok("lloyd.c", 'a'), sizeof(infoLloyd), IPC_CREAT | IPC_EXCL | 0600);
   if(shm < 0){
     perror("shmget");
     return ERR_OUT;
   }
   //shmat
-  memComp = shmat(shm, 0, 0)
+  memComp = shmat(shm, 0, 0);
   if (memComp == NULL){
     perror("shmat");
     return ERR_OUT;
   }
 
   //Inicialitzem semàfors
-  int s = SEM_constructor_with_name(semJack, "JackLloyd");
+  int s = SEM_constructor_with_name(&semJack, 'J');
   if (s == 0){
-    write(1, SEM_CREATE_ERROR);
+    write(1, SEM_CREATE_ERROR, sizeof(SEM_CREATE_ERROR));
     return ERR_OUT;
   }
 
-  s = SEM_init(semJack, 0);
+  s = SEM_init(&semJack, 0);
   if (s == 0){
-    write(1, SEM_CREATE_ERROR);
+    write(1, SEM_CREATE_ERROR, sizeof(SEM_CREATE_ERROR));
     return ERR_OUT;
   }
 
-
-  void inicialitzaAcum(int index){
-    infoAcumulada[index].temperatura = 0;
-    infoAcumulada[index].humitat = 0;
-    infoAcumulada[index].pressio_atmosferica = 0;
-    infoAcumulada[index].precipitacio = 0;
-  }
   //Reassignem signals
-  signalhandler(SIGINT, signalhandler);
+  signal(SIGINT, signalhandler);
 
   while (1) {
-    SEM_wait(semJack);
+    SEM_wait(&semJack);
     //Guardem info i calculem mitjana.
     //Comprovem si tenim l'estació
     if (numEstacions != 0) {
@@ -140,9 +140,9 @@ int main(){
       if (estacio == -1){
         //Creem una nova
         numEstacions++; //Augmentem el nombre de estacions existents
-        estacions = (infoLloyd *) realloc(sizeof(infoLloyd)*numEstacions); //Reservem espai per la nova estacio
-        infoAcumulada = (infoLloyd *) realloc(sizeof(infoLloyd) * numEstacions); //Reservem espai per la nova estacio a l'acumulat
-        numDades = (int *) realloc(sizeof(int)*numEstacions); //Reservo espai per el nombre de dades/estacio
+        estacions = (infoLloyd *) realloc(estacions, sizeof(infoLloyd)*numEstacions); //Reservem espai per la nova estacio
+        infoAcumulada = (infoLloyd *) realloc(infoAcumulada, sizeof(infoLloyd) * numEstacions); //Reservem espai per la nova estacio a l'acumulat
+        numDades = (int *) realloc(numDades, sizeof(int)*numEstacions); //Reservo espai per el nombre de dades/estacio
         numDades[numEstacions-1] = 1;
         //Inicialitzar infoAcumulada
         inicialitzaAcum(numEstacions-1);
@@ -152,7 +152,7 @@ int main(){
       }else{
         //Fer un guarda dades acumulades
         numDades[estacio]++;
-        infoAcumulada[estacio] = (infoLloyd *) realloc(sizeof(infoLloyd)*numDades[estacio]);
+        infoAcumulada[estacio] = (infoLloyd *) realloc(infoAcumulada, sizeof(infoLloyd)*numDades[estacio]);
         guardaDadesAcumulades(estacio);
         calculaMitjana(estacio);
       }
@@ -169,8 +169,8 @@ int main(){
       guardaDadesAcumulades(numEstacions-1);
       guardaDadesMitjana(numEstacions-1);
     }
-    SEM_signal(semJack);
+    SEM_signal(&semJack);
   }
 
-  return 0
+  return 0;
 }
